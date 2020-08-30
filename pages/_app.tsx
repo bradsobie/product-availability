bdeb5c6502a507053562fb5be9d22fdc38fb9bda
Bradley Sobie <bradsobie@gmail.com>
2020-08-30 17:19:47 -0500

    move global stuff to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import NextApp from 'next/app'
 import React from 'react'
 import { ThemeProvider as StyledThemeProvider } from 'styled-components'
 import { ThemeProvider as MaterialThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Head from 'next/head';
+import { GlobalStyle } from '../src/GlobalStyles';
 
 const theme = {
   ...createMuiTheme()
@@ -20,6 +22,18 @@ export default class App extends NextApp {
     return (
       <StyledThemeProvider theme={theme}>
         <MaterialThemeProvider theme={theme}>
+          <Head>
+            <title>Product Availability</title>
+            <link rel="icon" href="/favicon.svg" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+            <script dangerouslySetInnerHTML={{ __html: `
+              window.initMap = function(){}
+            `}} />
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhWfPD2n4uuD_cHbASZJQncDY1RQOBKpM&callback=initMap&libraries=places"></script>
+          </Head>
+
+          <GlobalStyle />
+
           <Component {...pageProps} />
         </MaterialThemeProvider>
       </StyledThemeProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { Container } from '@material-ui/core';
@@ -6,7 +5,6 @@ import { usePosition } from 'use-position';
 
 import firebase from '../src/firebase';
 import SearchByStore from '../src/SearchByStore';
-import { GlobalStyle } from '../src/GlobalStyles';
 import { Header } from '../src/Header';
 import { PlaceCard } from '../src/PlaceCard';
 
@@ -38,28 +36,14 @@ const Search = () => {
   const latLng = typeof window !== 'undefined' && (window as any)?.google?.maps ? new (window as any).google.maps.LatLng(latitude, longitude) : undefined;
 
   return <div>
-    <Head>
-      <title>Product Availability</title>
-      <link rel="icon" href="/favicon.svg" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-      <script dangerouslySetInnerHTML={{ __html: `
-        window.initMap = function(){}
-      `}} />
-      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhWfPD2n4uuD_cHbASZJQncDY1RQOBKpM&callback=initMap&libraries=places"></script>
-    </Head>
-
-    <GlobalStyle />
-
     <Container maxWidth="sm">
       <Header />
-
       <SearchByStore
         inputText={inputText}
         location={latLng}
         onChange={text => setInputText(text)}
         onSelect={onStoreSelected}
       />
-
       {inputText === '' && <PlaceCard place={selectedPlace} />}
     </Container>
   </div>
